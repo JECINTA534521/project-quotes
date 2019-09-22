6ada4b7ce67ce26ce5cccb8f13afd8a135c58509
jecinta <jecintawanjirug@gmail.com>
2019-09-22 23:30:59 +0300

    this adds the bootstrapMD and imports it

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { FormsModule} from '@angular/forms';
+// import { MDBBootstrapModule } from 'angular-bootstrap-md';
+
 import { AppComponent } from './app.component';
 import { QuoteComponent } from './quotes/quotes.component';
 import { QuotesDetailComponent } from './quotes-detail/quotes-detail.component';
 import { UnderlineDirective } from './underline.directive';
 import { DateCountPipe } from './date-counts.pipe';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
 
 @NgModule({
   declarations: [
@@ -14,11 +17,16 @@ import { DateCountPipe } from './date-counts.pipe';
     QuoteComponent,
     QuotesDetailComponent,
     UnderlineDirective,
-    DateCountPipe
+    DateCountPipe,
+    QuoteFormComponent
+
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    //  MDBBootstrapModule.forRoot()
+
   ],
   providers: [],
   bootstrap: [AppComponent]
